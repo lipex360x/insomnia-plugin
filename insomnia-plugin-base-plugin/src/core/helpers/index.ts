d2630c37a9c49bf8ca7445016f5b12dbc6d99ef9
Felipe Bambirra <lipex360@gmail.com>
2023-03-23 09:31:57 -0300

    project updated

--- insomnia-plugin-base-plugin/src/core/helpers/index.ts
+++ insomnia-plugin-base-plugin/src/core/helpers/index.ts
@@ -1,1 +1,1 @@
-export const bufferToJson = (buf:Buffer) => JSON.parse(buf.toString('utf-8'));
+export const bufferToJson = (buf: Buffer) => JSON.parse(buf.toString('utf-8'))
--- insomnia-plugin-base-plugin/src/main.ts
+++ insomnia-plugin-base-plugin/src/main.ts
@@ -1,39 +1,41 @@
-import { bufferToJson } from "./core/helpers";
+import { bufferToJson } from './core/helpers'
 
 export const requestHooks = [
   async (context: any) => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-    const data = await response.json();
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    const data = await response.json()
     context.store.setItem('itemId', JSON.stringify(data))
 
-    debugger
-  }
-];
+    // debugger
+  },
+]
 
 export const responseHooks = [
   async (context: any) => {
     const store = await context.store.getItem('myItem')
 
     const resp = bufferToJson(context.response.getBody())
-    
+
     console.log('body', resp)
 
     console.log('response', JSON.parse(store))
 
-    debugger
-  }
+    // debugger
+  },
 ]
 
-export const templateTags = [{
-  name: 'getKeyInStore',
-  displayName: 'Store Value',
-  args: [{ type: 'string', defaultValue: 'itemId' }],
+export const templateTags = [
+  {
+    name: 'getKeyInStore',
+    displayName: 'Store Value',
+    args: [{ defaultValue: 'itemId' }],
 
-  async run (context: any, theKey: string) {
+    async run(context: any, theKey: string) {
       const response = await context.store.getItem(theKey)
       const data = JSON.parse(response)
-      
+
       console.log('Tag Data', data)
       return data.id
-  }
-}];
+    },
+  },
+]
